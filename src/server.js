1b285d1cbeb17297a0146424ae5eaf20c7616629
wolfx <wolfx@wolfxs-MacBook-Pro.local>
2018-09-30 20:43:40 -0300

    heroku trails part 378173829173217837189374

--- src/server.js
+++ src/server.js
@@ -26,6 +26,13 @@ app.use(forceSSL());
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
