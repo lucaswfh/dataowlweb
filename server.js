e04710aa344062a48d4a6c06b5edb5ae36726fcd
wolfx <wolfx@wolfxs-MacBook-Pro.local>
2018-10-05 14:01:46 -0300

    heroku trails

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
