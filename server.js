f63c9a5c2e4c1a557b9ffdef2e70826d4bbcc9bc
wolfx <wolfx@wolfxs-MacBook-Pro.local>
2018-10-05 14:08:44 -0300

    heroku trails

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 // express server used to serve angular files in heroku 
+
+// requires 
+const path = require('path');
 const express = require('express');
+
+// express app 
 const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default
 // Heroku port
